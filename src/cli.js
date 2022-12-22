355d48da057e41c092147391d9bcfbdeac0ab90d
Danil Volkov <volchara287@gmail.com>
2022-12-22 18:30:01 +0600

    Добавил запрос имени у пользователя

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const myFunc = () => {
+    const name = readlineSync.question('Как тебя зовут? ')
+    console.log(`Hello, ${name}!`);
+} 
+
+export default myFunc;
