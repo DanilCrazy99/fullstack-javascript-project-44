093bf2b574c4f96dae828f3e3c0324e27a1fa61f
Danil Volkov <volchara287@gmail.com>
2022-12-26 18:09:27 +0600

    Правка кода для удобства дальнейшего использования

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import myFunc from '../src/cli.js';
 
-console.log('Brain-games.js');
-console.log('Welcome to the Brain Games!');
 myFunc();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const myFunc = () => {
+  console.log('Welcome to the Brain Games!');
   const namePlayer = readlineSync.question("What's your name? ");
   console.log(`Hello, ${namePlayer}!`);
   return namePlayer;
