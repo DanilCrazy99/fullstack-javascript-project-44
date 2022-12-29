7f90b6e6dc2b15af8e3143e0a4a5efdb96a0a1be
Danil Volkov <volchara287@gmail.com>
2022-12-29 14:40:15 +0600

    Рефакторинг: убрал повторы кусков кода

--- src/lbg_gcd.js
+++ src/lbg_gcd.js
@@ -6,33 +6,27 @@ import countRounds from './games_settings.js';
 
 const gameGCD = (namePlayer = 'Player') => {
   const Denominator = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
+  const findTempDenominator = (num, arrayTempDenominator) => {
+    let tempNum = num;
+    while (tempNum !== 1) {
+      for (let a = 0; a < Denominator.length; a += 1) {
+        if (tempNum % Denominator[a] === 0) {
+          tempNum /= Denominator[a];
+          arrayTempDenominator.push(Denominator[a]);
+          break;
+        }
+      }
+    }
+  };
   console.log('Find the greatest common divisor of given numbers.');
   for (let i = 0; i <= countRounds - 1; i += 1) {
     const tempDenominator1 = [];
     const tempDenominator2 = [];
     const num1 = Math.floor(Math.random() * 10 + Math.random() * 10);
-    let tempNum1 = num1;
     const num2 = Math.floor(Math.random() * 10 + Math.random() * 10);
-    let tempNum2 = num2;
     let result = 1;
-    while (tempNum1 !== 1) {
-      for (let a = 0; a < Denominator.length; a += 1) {
-        if (tempNum1 % Denominator[a] === 0) {
-          tempNum1 /= Denominator[a];
-          tempDenominator1.push(Denominator[a]);
-          break;
-        }
-      }
-    }
-    while (tempNum2 !== 1) {
-      for (let b = 0; b < Denominator.length; b += 1) {
-        if (tempNum2 % Denominator[b] === 0) {
-          tempNum2 /= Denominator[b];
-          tempDenominator2.push(Denominator[b]);
-          break;
-        }
-      }
-    }
+    findTempDenominator(num1, tempDenominator1);
+    findTempDenominator(num2, tempDenominator2);
     for (let c = tempDenominator1.length; c > 0; c -= 1) {
       for (let d = tempDenominator2.length; d > 0; d -= 1) {
         if (tempDenominator1[c - 1] === tempDenominator2[d - 1]) {
