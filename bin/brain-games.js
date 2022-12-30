d1142a137f98caf15bbf58e47c04b1d2057cef31
Danil Volkov <volchara287@gmail.com>
2022-12-30 15:55:37 +0600

    Изменение приветственного сообщения

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 import { Greeting } from '../src/index.js';
 
+console.log('Welcome to the Brain Games!');
 Greeting();
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import readlineSync from 'readline-sync';
 // Приветствие игрока
 const Greeting = () => {
   console.log('Welcome to the Brain Games!');
-  const namePlayer = readlineSync.question("What's your name? ");
+  const namePlayer = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${namePlayer}!`);
   return namePlayer;
 };
