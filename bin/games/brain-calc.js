0a6aeb19b2576bae22810dd81fcfec6661a2bca3
Danil Volkov <volchara287@gmail.com>
2023-01-06 14:30:16 +0600

    Исправил импорт в файлах

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import Greeting from '../../src/cli.js';
+import { Greeting } from '../../src/cli.js';
 import CalcGame from '../../src/game-logic/logic_calc.js';
 
 const namePlayer = Greeting();
