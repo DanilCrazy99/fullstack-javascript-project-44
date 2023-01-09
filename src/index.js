7ace90922488021439a6cb6b1783f028ca37e803
Danil Volkov <volchara287@gmail.com>
2023-01-09 14:59:05 +0600

    I в цикле, return вместо break

--- src/index.js
+++ src/index.js
@@ -5,21 +5,20 @@ const ENGINE = (gameDescription, newRound) => {
   console.log('Welcome to the Brain Games!');
   const namePlayer = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${namePlayer}!`);
-  let i = 0;
   console.log(gameDescription);
-  for (; i < countRounds; i += 1) {
+  for (let i = 0; i < countRounds; i += 1) {
     const [question, result] = newRound();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
-    if (String(result) !== String(answer)) {
+    if (result !== answer) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.`);
-      console.log("Let's try again!");
-      break;
+      return console.log("Let's try again!");
+    }
+    if (i === countRounds) {
+      console.log('Congratulations!');
     }
   }
-  if (i === countRounds) {
-    console.log('Congratulations!');
-  }
+  return 0;
 };
 
 export default ENGINE;
