7ad5a505c7bf5f49b2097a3c4a391a4cb425be85
Danil Volkov <volchara287@gmail.com>
2023-01-09 14:50:19 +0600

    Удаление ненужных объявлений функций

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import readlineSync from 'readline-sync';
 import { countRounds } from './games_settings.js';
-import { Mistake, Congratulations } from './cli.js';
 
-const Greeting = () => {
+const ENGINE = (gameDescription, newRound) => {
   console.log('Welcome to the Brain Games!');
   const namePlayer = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${namePlayer}!`);
-};
-
-const ENGINE = (gameDescription, newRound) => {
-  Greeting();
   let i = 0;
   console.log(gameDescription);
   for (; i < countRounds; i += 1) {
@@ -17,12 +12,13 @@ const ENGINE = (gameDescription, newRound) => {
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (String(result) !== String(answer)) {
-      Mistake(answer, result);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.`);
+      console.log("Let's try again!");
       break;
     }
   }
   if (i === countRounds) {
-    Congratulations();
+    console.log('Congratulations!');
   }
 };
 
