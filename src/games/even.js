17108ac01a5481b033995b9a7113b23a7fdb0e01
Danil Volkov <volchara287@gmail.com>
2023-01-11 17:30:36 +0600

    Вынос в функцию проверки на чётность

--- src/games/even.js
+++ src/games/even.js
@@ -5,15 +5,19 @@ import ENGINE from '../index.js';
 
 const rulesGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const generateRound = () => {
+const checkNumIsEven = (num) => {
   let result = '';
-  const num = getRandomNum(1, 20);
   if (num % 2) {
     result = 'no';
   } else {
     result = 'yes';
   }
-  return [num, result];
+  return result;
+};
+
+const generateRound = () => {
+  const num = getRandomNum(1, 20);
+  return [num, checkNumIsEven(num)];
 };
 
 export default () => { ENGINE(rulesGame, generateRound); };
