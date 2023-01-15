6962e90d01b7e4c09ee718e6e45ddf78cd8da748
Danil Volkov <volchara287@gmail.com>
2023-01-15 23:47:07 +0600

    Переделал функцию для поиска gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,47 +2,20 @@
 
 import engine from '../index.js';
 import getRandomNum from '../utils.js';
-import { primeNumbers } from '../games_settings.js';
+// import { primeNumbers } from '../games_settings.js';
 
 const rules = 'Find the greatest common divisor of given numbers.';
 
-const findTempDenominator = (num) => {
-  const arrayTempDenominator = [];
-  let tempNum = num;
-  while (tempNum !== 1) {
-    for (let a = 0; a < primeNumbers.length; a += 1) {
-      if (tempNum % primeNumbers[a] === 0) {
-        tempNum /= primeNumbers[a];
-        arrayTempDenominator.push(primeNumbers[a]);
-        break;
-      }
-    }
-  }
-  return arrayTempDenominator;
-};
-
-const findGCDtwoNumbers = (tempDenominator1, tempDenominator2) => {
-  const Denominator1 = tempDenominator1;
-  const Denominator2 = tempDenominator2;
-  let result = 1;
-  for (let c = Denominator1.length; c > 0; c -= 1) {
-    for (let d = Denominator2.length; d > 0; d -= 1) {
-      if (Denominator1[c - 1] === Denominator2[d - 1]) {
-        result *= Denominator2[d - 1];
-        Denominator1[c - 1] = 1;
-        Denominator2[d - 1] = 1;
-      }
-    }
-  }
-  return result;
+const getGCD = (num1, num2) => {
+  if (num1 === 0) {
+    return num2;
+  } return getGCD(num2 % num1, num1);
 };
 
 const generateRound = () => {
   const num1 = getRandomNum(0, 20);
   const num2 = getRandomNum(0, 20);
-  const arrDenominatorFirstNum = findTempDenominator(num1);
-  const arrDenominatorSecondNum = findTempDenominator(num2);
-  const result = String(findGCDtwoNumbers(arrDenominatorFirstNum, arrDenominatorSecondNum));
+  const result = String(getGCD(num1, num2));
   const question = `${num1} ${num2}`;
   return [question, result];
 };
