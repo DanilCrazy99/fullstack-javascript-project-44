18f1c4d0499a6bd8db11c8426acddf920f0d41b7
Danil Volkov <volchara287@gmail.com>
2023-01-20 23:32:34 +0600

    Убрал ненужные сложные функции

--- src/games/progression.js
+++ src/games/progression.js
@@ -11,35 +11,18 @@ const generateProgression = (startNum, lenProgression, steps) => {
   for (let a = 1; a < lenProgression; a += 1) {
     progressionArr.push(startNum + a * steps);
   }
-  return progressionArr;
-};
-
-const findMissingElement = (arrProgression) => {
-  const arrWithMissingNum = arrProgression;
-  const randomCellArr = getRandomNum(0, arrProgression.length - 1);
-  const result = String(arrProgression[randomCellArr]);
-  arrWithMissingNum[randomCellArr] = '..';
-  return [result, arrWithMissingNum];
-};
-
-const normalString = (arrQuestion) => {
-  let question = '';
-  for (let a = 0; a < arrQuestion.length; a += 1) {
-    question += arrQuestion[a];
-    if (a < arrQuestion.length - 1) {
-      question += ' ';
-    }
-  }
-  return question;
+  const randomPlaceInArr = getRandomNum(0, progressionArr.length - 1);
+  const result = String(progressionArr[randomPlaceInArr]);
+  progressionArr[randomPlaceInArr] = '..';
+  return [progressionArr, result];
 };
 
 const generateRound = () => {
   const lenProgression = getRandomNum(5, lengthProgression);
   const startNum = getRandomNum(1, 10);
   const progressionStep = getRandomNum(1, 10);
-  const progressionArr = generateProgression(startNum, lenProgression, progressionStep);
-  const [result, arrMissNum] = findMissingElement(progressionArr);
-  const question = normalString(arrMissNum);
+  const [progressionArr, result] = generateProgression(startNum, lenProgression, progressionStep);
+  const question = progressionArr.join(' ');
   return [question, result];
 };
 
